21c0fc17c421fa3abe7f6a2460c9b005205d0175
Novaleaf <jasons@novaleaf.com>
2023-01-15 17:38:25 -0800

    finish adding docs and tweak gravity / jump speeds to not require turbo speed

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+/** FOOT SMASH GAME!  a clone of DiveKick, but uses only one button per player (A or B) */
 namespace FootSmash {
     //a namespace is like a box, containing other things (code, variables, classes, other namespaces)
     //everything inside a namespace (box) can see everything else inside. 
@@ -7,8 +8,10 @@ namespace FootSmash {
 
     /** how fast the game runs.  increase to simulate "turbo mode"
      * if your algorithms are done properly, changing this should not impact physics simulation / gameplay logic
+     * apply a game speed modifier to make the game seem faster.
+     * this is needed because the game's clock seems slow.  maybe runs 1/10 normal.
      *  */
-    let GAME_SPEED = 10.0 //default normal = 5.0
+    let GAME_SPEED = 1.0 //default normal = 1.0
     /** y position where the game's "ground" should be */
     let GROUND = 120
     /** screen x position of the left game border */
@@ -16,7 +19,7 @@ namespace FootSmash {
     /** screen x position of the game border  */
     let WALL_RIGHT = 155;
     /** constant acceleration when jumping */
-    let GRAVITY = -9.8
+    let GRAVITY = -300.0
 
     /** helper to throw an error (for debugging purposes) 
      * and still see the proper line number in the stack trace at the bottom of the IDE.
@@ -42,7 +45,7 @@ namespace FootSmash {
     /** simple "finite state machine" (FSM) used for splitting up the player's game mechanics up into their own modular parts
      * more on FSM: https://www.youtube.com/watch?v=-ZP2Xm-mY4E   and http://gameprogrammingpatterns.com/state.html
       */
-    namespace PlayerFSM {
+    namespace PlayerActivityFsm {
 
         /** A State holds all the variables and decisions (logic and state) for the owner when they are in that state.         * 
          * This IState is the minimum functionality we need to have a full FSM.
@@ -73,7 +76,7 @@ namespace FootSmash {
 
                 //if player below ground, teleport to ground
                 if (owner.height < 0) {
-                    owner.standY(0);
+                    owner.SetPositionStandY(0);
                 }
                 //if player above ground, start falling to ground
                 if (owner.height > 0) {
@@ -82,7 +85,7 @@ namespace FootSmash {
 
                 //if player presses action, JUMP!
                 if (owner.actionButton.isPressed() === true) {
-                    return new JumpingState(owner, owner.speed);
+                    return new JumpingState(owner, owner.jumpVelocity);
                 }
 
                 //nothings happening, just return ourself so next game update this Idle state runs again
@@ -119,8 +122,10 @@ namespace FootSmash {
 
             }
 
+            private totalElapsed = 0;
             public update(elapsedSec: number, owner: Player): IState<Player> {
-
+                this.totalElapsed += elapsedSec;
+                console.log(this.totalElapsed);
                 //move up/down by gravity
                 {
                     //apply velocity to position.  our game only has y (height) velocity.
@@ -128,11 +133,11 @@ namespace FootSmash {
                     //apply acceleration to velocity
                     this.yVelocity += GRAVITY * elapsedSec;
                     //apply newly calculated position to owner player's state.  (update sprites too)
-                    owner.standY(newHeight);
+                    owner.SetPositionStandY(newHeight);
 
                     //if at or below zero, back to standing
                     if (newHeight <= 0) {
-                        owner.standY(0);
+                        owner.SetPositionStandY(0);
                         return new IdleState();
                     }
                 }
@@ -181,29 +186,48 @@ namespace FootSmash {
         }
 
     }
+
+    /** Manages the logic for a player.  called every engine.update() to check for player input and switch between FSM states. */
     class Player {
 
+        /** when jumping, how fast/far we spring upwards.  38.0 is about full screen vertical distance */
+        public jumpVelocity = 200.0;
+
+        /** height above ground.  private so things outside our class can not modify this directly. */
         private _height = 0;
         /** 
+         * lets code outside this Player class read the height variable without being able to change it.
+         * changing has to be done via one of the methods like .SetPositionStand()
+         * 
          * get/set properties in typescript: https://www.typescriptlang.org/docs/handbook/2/classes.html#getters--setters
          */
         public get height() { return this._height; }
 
-        //private set height(value:number){this._height = value; }
-        speed = 38.0;
+
+        /** sprites used by this player.  WILL BE CHANGED LATER to support multiple costumes for each. (idle animations, etc) 
+         *  TODO: move to animation subsystem
+        */
         public head: Sprite;
         public body: Sprite;
         public foot: Sprite;
         public diveBody: Sprite;
-        /** when this changes, sprites will be flipped */
+
+
+        /** when this changes, sprites will be flipped direction  TODO: move to animation subsystem*/
         public isFacingRight: boolean = true;
 
+        /** the button that controls this player. */
         public actionButton: controller.Button;
 
-        public currentState: PlayerFSM.IState<Player>;
+        /** the FSM dealing with player activity states
+         * The <Player> means that the owner is always a Player object
+         */
+        public currentActivityState: PlayerActivityFsm.IState<Player>;
 
-        constructor(public isP1: boolean, public xPos: number) {
+        /** the constructor is automatically called when the Player object is created ([new] called) */
+        constructor(/**true if player1, false if player2 */public isP1: boolean,/** players current X position */ public xPos: number) {
 
+            /** set different sprites/button for each player */
             if (isP1) {
                 this.body = sprites.create(assets.image`Body1`, PlayerSpriteKind.P1_Body);
                 this.diveBody = sprites.create(assets.image`DiveBody1`, PlayerSpriteKind.P1_Body);
@@ -221,25 +245,26 @@ namespace FootSmash {
                 this.actionButton = controller.A;
             }
 
-            //hide
+            //hide the dive body sprite, as it's not used to start with   TODO: move to animation subsystem
             this.diveBody.setPosition(-100, -100);
 
-            //set position;
-            this.stand(xPos, 0);
-
-            this.currentState = new PlayerFSM.IdleState();
-
-
-
+            //set position, starting Y is always on the ground;
+            this.SetPositionStand(xPos, 0);
+            //set the default player activity to IDLE
+            this.currentActivityState = new PlayerActivityFsm.IdleState();
         }
 
+        /** called every engine.update(), lets the player check input, calculate state changes, animate, etc */
         public update(elapsedSec: number) {
-            this.currentState = this.currentState.update(elapsedSec, this);
+            //updates the player activity FSM, which will return whatever State
+            //that should be run next update (maybe the same state)
+            this.currentActivityState = this.currentActivityState.update(elapsedSec, this);
+            //update animations
             this._updateSpriteDirections();
         }
-
+        /**  TODO: move to animation subsystem */
         private _updateSpriteDirections() {
-            //get facing direction of this player
+            //get/update facing direction of this player
             {
                 let newIsFacingRight = false;
                 //first get facing of p1
@@ -252,6 +277,8 @@ namespace FootSmash {
                     //if actually p2, just switch facing
                     newIsFacingRight = !newIsFacingRight;
                 }
+
+                //if our facing just flipped, update sprites and store the new facing value
                 if (newIsFacingRight != this.isFacingRight) {
                     this.isFacingRight = newIsFacingRight;
                     //flip all sprites
@@ -263,7 +290,8 @@ namespace FootSmash {
             }
         }
 
-        public stand(xPos: number, height: number): void {
+        /** change the players position and set to standing (idle) sprites */
+        public SetPositionStand(xPos: number, height: number): void {
             this.xPos = xPos;
             this._height = height;
 
@@ -274,43 +302,64 @@ namespace FootSmash {
             this.foot.setPosition(xPos, y - 5);
         }
         /** set character to standing, only passing in a height */
-        public standY(height: number): void {
-            this.stand(this.xPos, height);
+        public SetPositionStandY(height: number): void {
+            //just call the other Stand method, passing in the existing xPos
+            this.SetPositionStand(this.xPos, height);
         }
     }
-
-    export class MyGame {
+    /** our simple game engine for FOOTSMASH!   
+     * creates two players that can jump and divekick, all being controlled by a single button.
+     * after you create this, you need to call the .pumpUpdate() method in a [forever] block
+      */
+    export class EntryPoint {
+        /** players are put against the walls */
         public p1 = new Player(true, WALL_LEFT);
         public p2 = new Player(false, WALL_RIGHT);
+
+        /** store the last time the [pumpUpdate()] ran, used for calculating elapsed time between called */
         private lastUpdateTimestamp: number;
 
         constructor() {
             this.lastUpdateTimestamp = game.runtime();
         }
 
-
+        /** updates the different "systems" of the game.
+         * This is done in discrete steps, each taking a small (but variable) amount of time, about 33ms (1/30th of a second).
+         * because this amount of time is so small, we can move things across the screen or look for player presses, 
+         * and it seems to happen smoothly (for moving sprites) or instantly (reacting to player input) * 
+         */
         public pumpUpdate() {
             //calculate time since last call to this function
             let currentTimestamp = game.runtime();
             let elapsedMs = currentTimestamp - this.lastUpdateTimestamp;
-            let elapsedSec = (elapsedMs / 1000.0) * GAME_SPEED;
+            /**this is how many seconds since last call to [pumpUpdate]*/
+            let elapsedSec = (elapsedMs / 1000.0);
+            //apply a game speed modifier to make the game seem faster.  
+            //this is needed because the game's clock seems slow.  maybe runs 1/10 normal.
+            elapsedSec *= GAME_SPEED;
 
-
+            //update our various systems with this calculated [elapsedSec]
             this.update(elapsedSec);
 
-
+            //store our update time for calculation next loop
             this.lastUpdateTimestamp = currentTimestamp;
         }
 
         private update(elapsedSec: number) {
+            //let our players update themselves  
+            //(pick what state they should be in, update their animations, physics, collision detection, etc)
             this.p1.update(elapsedSec);
             this.p2.update(elapsedSec);
         }
     }
 
 }
-let myGame = new FootSmash.MyGame();
+
+//this section executes when this file is loaded up, acting as the "entry point" into the game.
+
+/** create a instance of our little footsmash game */
+let myGame = new FootSmash.EntryPoint();
 // hooks into the makecode arcade engine to get an update loop
-forever(function () {
+game.onUpdate(function () {
     myGame.pumpUpdate();
 })
